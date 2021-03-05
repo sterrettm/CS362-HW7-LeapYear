1f2d9ef6c9f8ce28e27467d0f4cfcc9c931ae8c3
Matthew Sterrett <sterretm@oregonstate.edu>
2021-03-04 21:39:41 -0800

    Added to leapyear, implemented correct behavior for years that are multiples of 100 (but not 400)

--- leapyear.py
+++ leapyear.py
@@ -1,6 +1,9 @@
 
 def leapyear(year):
     if (year % 4 == 0):
-        return True
+        if (year % 100 == 0):
+            return False
+        else:
+            return True
     else:
         return False
